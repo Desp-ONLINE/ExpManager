fe37d01b7b1c3e0aa2fe3ae86f8756d8fe8e7639
khw3268480 <khw3268480@naver.com>
2025-04-10 15:49:02 +0900

    명령어 샌더 수정

--- src/main/java/EXPManager/command/ElixirFormulaCommand.java
+++ src/main/java/EXPManager/command/ElixirFormulaCommand.java
@@ -15,8 +15,9 @@ public class ElixirFormulaCommand implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         Player player = (Player) commandSender;
         player.sendMessage("§a 경험치 공식은 다음과 같습니다.");
+        player.sendMessage("");
         player.sendMessage("§f 몬스터 경험치 = §2M§f, 경험치 비약 = §dP§f, 경험치 이벤트 = §aE§f, 추가 경험치 스텟 = §6S§f");
-        player.sendMessage("§f 최종 경험치 지급량: (§2M§f*§6S§f)*(§dP§f+§aE)");
+        player.sendMessage("§f 최종 경험치 지급량: (§2M§f*§6S§f)*(§dP§f+§aE§f)");
         return false;
     }
 }
--- src/main/java/EXPManager/command/EventCommand.java
+++ src/main/java/EXPManager/command/EventCommand.java
@@ -7,25 +7,38 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class EventCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        Player player = (Player) commandSender;
-        if(!player.isOp()){
-            return false;
+
+        if(commandSender instanceof Player){
+            Player player = (Player) commandSender;
+            if(!player.isOp()){
+                return false;
+            }
+            if(strings.length <= 1){
+                player.sendMessage("§c잘못된 용법: /경험치이벤트 <배수(%단위)> <시간(초단위)> ");
+                return false;
+            }
+            Integer multiply = Integer.valueOf(strings[0]);
+            Integer duration = Integer.valueOf(strings[1]);
+            EventRepository.getInstance().startEvent(multiply, duration);
+            TextComponent text = Component.text("§a 경험치 이벤트가 시작되었습니다! §f" + duration + "§a초 동안 §f" + multiply + "% §a만큼 경험치가 추가지급 됩니다.");
+            Bukkit.getServer().broadcast(text);
+            return true;
         }
-        if(strings.length <= 1){
-            player.sendMessage("§c잘못된 용법: /경험치이벤트 <배수(%단위)> <시간(초단위)> ");
-            return false;
+        if(commandSender instanceof ConsoleCommandSender){
+            Integer multiply = Integer.valueOf(strings[0]);
+            Integer duration = Integer.valueOf(strings[1]);
+            EventRepository.getInstance().startEvent(multiply, duration);
+            TextComponent text = Component.text("§a 경험치 이벤트가 시작되었습니다! §f" + duration + "§a초 동안 §f" + multiply + "% §a만큼 경험치가 추가지급 됩니다.");
+            Bukkit.getServer().broadcast(text);
+            return true;
         }
-        Integer multiply = Integer.valueOf(strings[0]);
-        Integer duration = Integer.valueOf(strings[1]);
-        EventRepository.getInstance().startEvent(multiply, duration);
-        TextComponent text = Component.text("§a 경험치 이벤트가 시작되었습니다! §f" + duration + "§a초 동안 §f" + multiply + "% §a만큼 경험치가 추가지급 됩니다.");
-        Bukkit.getServer().broadcast(text);
         return true;
     }
 }
